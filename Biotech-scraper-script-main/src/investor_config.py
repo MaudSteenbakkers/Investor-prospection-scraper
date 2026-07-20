534bf8904960e103fd4c334324aa604fbffee2a6
Maud Steenbakkers <MSteenbakkers@innoserlaboratories.com>
2026-07-20 17:12:12 +0200

    Update investor_config.py

--- Biotech-scraper-script-main/src/investor_config.py
+++ Biotech-scraper-script-main/src/investor_config.py
@@ -1,1 +1,10 @@
-
+TARGET_FIELDS = [
+    "main_sector",
+    "therapeutic_areas",
+    "investment_stage",
+    "regions",
+    "investment_thesis",
+    "ticket_size_min",
+    "ticket_size_max",
+    "additional_notes",
+]
